e16fde7231826a54204e378f41f3a3b39137078c
unknown <sam7073@naver.com>
2020-09-26 03:31:33 +0900

    9/26 data visualization assignment completed

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, StatusBar } from 'react-native';
+import {SafeAreaView, StyleSheet, ScrollView, StatusBar} from 'react-native';
 
-import { Header, Colors } from 'react-native/Libraries/NewAppScreen';
+import {Header, Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App: () => React$Node = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
