33c29b6e90c81f55bf42b6306b2a06c3a6b7d5fa
unknown <sam7073@naver.com>
2020-10-01 22:23:08 +0900

    10/1 capstone data preprocessing

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Button, TouchableOpacity, StyleSheet, View} from 'react-native';
-import {RNCamera} from 'react-native-camera';
+import { Button, TouchableOpacity, StyleSheet, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
 const App: () => React$Node = () => {
   const cameraRef = React.useRef(null);
@@ -17,7 +17,7 @@ const App: () => React$Node = () => {
   return (
     <>
       <RNCamera
-        style={{width: 300, height: 300}}
+        style={{ width: 300, height: 300 }}
         type={RNCamera.Constants.Type.back}
         captureAudio={false}
       />
@@ -33,7 +33,7 @@ const styles = StyleSheet.create({
   button: {
     alignItems: 'center',
     backgroundColor: '#222222',
-    padding: 20,
+    padding: 30,
   },
 });
 export default App;
