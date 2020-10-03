bc5f519d034abdec5c64c5b1ae06f9b0a5872423
unknown <sam7073@naver.com>
2020-10-03 16:26:12 +0900

    10/3 study check

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Button, TouchableOpacity, StyleSheet, View } from 'react-native';
-import { RNCamera } from 'react-native-camera';
+import {Button, TouchableOpacity, StyleSheet, View} from 'react-native';
+import {RNCamera} from 'react-native-camera';
 const App: () => React$Node = () => {
   const cameraRef = React.useRef(null);
   const takePhoto = async () => {
@@ -16,7 +16,7 @@ const App: () => React$Node = () => {
   return (
     <>
       <RNCamera
-        style={{ width: 300, height: 300 }}
+        style={{width: 300, height: 300}}
         type={RNCamera.Constants.Type.back}
         captureAudio={false}
       />
